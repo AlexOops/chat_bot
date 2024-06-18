dd40b411b5f0b94d06178e8ac4107677038472ce
AlexOops <8768905213@mail.ru>
2024-06-18 12:18:47 +0300

    redesigned (voice + text)

---
+++ src/bot.js
@@ -0,0 +1,85 @@
+import {Telegraf, session} from 'telegraf';
+import {message} from 'telegraf/filters';
+import {code} from 'telegraf/format';
+import {ogg} from './oggConvertor.js';
+import {openaiService} from './openai.js';
+import {removeFile} from './utils.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const INITIAL_SESSION = {
+    messages: [],
+}
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.use(session());
+
+bot.command('start', async (ctx) => {
+    ctx.session = INITIAL_SESSION;
+    await ctx.reply('Привет! Я бот ChatGPT. Задай мне любой вопрос.');
+});
+
+bot.command('new', async (ctx) => {
+    ctx.session = INITIAL_SESSION;
+    await ctx.reply('Жду вашего текстовало или голосового сообщения!');
+});
+
+bot.on(message('voice'), async (ctx) => {
+    ctx.session ??= INITIAL_SESSION;
+
+    try {
+        await ctx.reply(code('Сообщение принял. Ожидайте ответа...'));
+
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        const userId = String(ctx.message.from.id);
+
+        const oggPath = await ogg.create(link.href, userId);
+        const mp3Path = await ogg.toMp3(oggPath, userId);
+
+        await removeFile(oggPath);
+
+        const text = await openaiService.transcription(mp3Path);
+
+        await ctx.reply(code(`Ваш запрос: ${text}`));
+
+        ctx.session.messages.push({role: openaiService.roles.USER, content: text});
+
+        const response = await openaiService.chat(ctx.session.messages);
+
+        ctx.session.messages.push({role: openaiService.roles.ASSISTANT, content: response});
+
+        await ctx.reply(response);
+
+    } catch (e) {
+        console.error(`Error while processing voice message`, e.message);
+        await ctx.reply('Произошла ошибка при обработке голосового сообщения.');
+    }
+});
+
+bot.on(message('text'), async (ctx) => {
+    ctx.session ??= INITIAL_SESSION;
+
+    try {
+        await ctx.reply(code('Сообщение принял. Ожидайте ответа...'));
+
+        ctx.session.messages.push({role: openaiService.roles.USER, content: ctx.message.text});
+
+        const response = await openaiService.chat(ctx.session.messages);
+
+        ctx.session.messages.push({role: openaiService.roles.ASSISTANT, content: response});
+
+        await ctx.reply(response);
+
+    } catch (e) {
+        console.log(`Error while text message`, e.message);
+    }
+});
+
+bot.launch();
+
+console.log('Bot is running...');
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
---
+++ src/openai.js
@@ -0,0 +1,60 @@
+import OpenAI from "openai";
+import {createReadStream} from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class OpenAIService {
+
+    roles = {
+        ASSISTANT: 'assistant',
+        USER: 'user',
+        SYSTEM: 'system',
+    }
+
+    constructor(apiKey) {
+        if (!apiKey) {
+            console.error('API key is not set.');
+            throw new Error('API key is required.');
+        }
+
+        this.openai = new OpenAI({apiKey});
+    }
+
+    async chat(messages) {
+        try {
+            const response = await this.openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: messages,
+            });
+            if (response.choices && response.choices.length > 0) {
+                return response.choices[0].message.content;
+            } else {
+                console.error('Unexpected response format:', response);
+                return null;
+            }
+        } catch (e) {
+            console.log('Error while creating chat completion:', e.message);
+            return null;
+        }
+    }
+
+    async transcription(filepath) {
+        try {
+            const transcription = await this.openai.audio.transcriptions.create({
+                file: createReadStream(filepath),
+                model: "whisper-1",
+                response_format: "text",
+            });
+
+            return transcription.text;
+
+        } catch (e) {
+            console.log('Error while transcription:', e.message);
+            return null;
+        }
+    }
+}
+
+const apiKey = process.env.OPENAI_KEY;
+export const openaiService = new OpenAIService(apiKey);
